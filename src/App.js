7c5122bb12e19d4ee2614d8a6ac60fc1dc89c1e5
NIALL MC DONAGH - STUDENT <G00383867@gmit.ie>
2021-10-06 11:35:14 +0100

    2nd DataRep lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+{/* Navigation bar gotten from Bootstrap from import of bootstrap up above */}
+<Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+       <Nav.Link href="/">Home</Nav.Link> {/*Puts a link between all the components and allows the route/switch to work */}
+      <Nav.Link href="/read">Read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
+    </Nav>
+  </Navbar>
+      <br></br>
+       <Switch> {/*Allows routing to the individual components like content etc. */}
+      <Route path='/' component={Content} exact />
+      <Route path='/create' component={Header} exact />
+      <Route path='/read' component={Footer} exact />
+      </Switch>
+        
+       
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
